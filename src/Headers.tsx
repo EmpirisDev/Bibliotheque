d3c406bda8d1637355381dbff405885b32142331
EmpirisDev <empiriss@gmail.com>
2023-03-29 22:29:08 +0200

    Création du Headers.tsx

--- src/Headers.tsx
+++ src/Headers.tsx
@@ -1,8 +1,35 @@
 export const Headers = () => {
     return (
-        <h1 className=" font-bold  text-center ">
-
-        </h1>
-    )
-
-}
+        <header className="bg-gray-100 ">
+            <div className="container mx-auto px-6 py-3">
+                <div className="  flex items-center justify-between">
+                    <div className="hidden md:flex">
+                        <div className="flex items-center">
+                            <a className="mr-8 text-gray-300 hover:text-white mr-4" href="#">
+                                <img className={"h-10 w-10 "} src="public/images/livre.svg" alt="Livre"/>
+                            </a>
+                            <a className="mr-8 text-gray-300 hover:text-white mr-4" href="#">
+                                <img className={"h-10 w-10 "} src="public/images/Collection.svg" alt="Série"/>
+                            </a>
+                            <a className="mr-8 text-gray-300 hover:text-white mr-4" href="https://open.spotify.com/">
+                                <img className={"h-10 w-10 "} src="public/images/bibliotheque.svg" alt="Bibliothèque"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="md:hidden">
+                        <button type="button" className="text-gray-500 hover:text-white focus:outline-none focus:text-white">
+                            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M3 5h18v2H3V5zm0 6h18v2H3v-2zm0 6h18v2H3v-2z"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <div className="hidden md:flex items-center">
+                        <a className="text-gray-300 hover:text-white mr-4" href="#">
+                            <img className={"h-10 w-10 "} src="public/images/profil.svg" alt="Profil"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
