333314ba3309beb75c0c67f09d8ffbe4d5d6da51
EmpirisDev <empiriss@gmail.com>
2023-03-29 19:51:05 +0200

    Update Headers.tsx

--- src/Headers.tsx
+++ src/Headers.tsx
@@ -1,6 +1,6 @@
 export const Headers = () => {
     return (
-        <h1 className="text-8xl pb-32 font-bold underline text-center">
+        <h1 className=" font-bold  text-center">
             Hello world!
         </h1>
     )
